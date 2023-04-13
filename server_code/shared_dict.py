61e7935d1a33bc8f0e60880e697b8701abcc92a5
Viktor Valadi <viktorvaladi@Viktors-MacBook-Pro.local>
2023-04-13 17:18:57 +0200

    debug

--- server_code/shared_dict.py
+++ server_code/shared_dict.py
@@ -1,38 +1,27 @@
+import ctypes
 import multiprocessing as mp
 import numpy as np
 
-
-# Create the shared dictionary in a separate process
 def create_shared_dict():
     # Define the dictionary
-    my_dict = {
-            #"agx4.nodes.edgelab.network" : 0, NOT WORKING ATM, fix it!! (flush and reinstall)
-            "agx6.nodes.edgelab.network" : 0,
-            "agx9.nodes.edgelab.network" : 0,
-            "agx10.nodes.edgelab.network" : 0,
-            "orin1.nodes.edgelab.network" : 0,
-            "orin2.nodes.edgelab.network" : 0
-        }
+    my_dict = {'key1': 'value1', 'key2': 'value2'}
     
     # Create a shared memory buffer to hold the dictionary
-    dt = np.dtype([('key', 'S'), ('value', np.int32)])
-    buffer = mp.RawArray('c', len(my_dict) * dt.itemsize)
-    arr = np.frombuffer(buffer, dtype=dt)
-    shared_dict = {arr[i]['key'].decode(): arr[i]['value'] for i in range(len(my_dict))}
+    buffer = mp.RawArray(ctypes.c_char, len(repr(my_dict).encode()))
     
     # Copy the dictionary into the shared memory buffer
-    shared_dict[:] = repr(my_dict).encode()
+    ctypes.memmove(buffer, repr(my_dict).encode(), len(repr(my_dict).encode()))
     
     return buffer
 
 # Use the shared dictionary from another process
 def use_shared_dict(buffer):
-    dt = np.dtype([('key', 'S'), ('value', np.int32)])
     # Access the shared memory buffer as a numpy array
-    arr = np.frombuffer(buffer, dtype=dt)
+    arr = np.frombuffer(buffer, dtype=np.uint8)
     shared_dict = arr.view(dtype=np.dtype('O')).reshape(1)[0]
     
     # Convert the shared dictionary back into a regular dictionary
     my_dict = eval(shared_dict.tobytes().decode())
     
+    # Access and modify the dictionary as needed
     return my_dict
