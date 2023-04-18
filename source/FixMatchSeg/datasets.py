5efb82d8e047a9a59c63e8c4f649019cc8a6ffa0
YazanGhafir <yazan.yasser.ghafir@gmail.com>
2023-04-18 14:51:10 +0200

    prepared Zod data for FixMatchSeg

---
+++ source/FixMatchSeg/datasets.py
@@ -0,0 +1,117 @@
+from static_params import *
+from GroundTruth.gt_utils import *
+
+class ZODImporter:
+    def __init__(
+        self,
+        root=DATASET_ROOT,
+        subset_factor=SUBSET_FACTOR,
+        img_size=IMG_SIZE,
+        batch_size=BATCH_SIZE,
+        tb_path=TB_PATH,
+    ):
+        self.zod_frames, self.training_frames_all, self.validation_frames_all = load_ego_road(root)
+
+        self.training_frames = self.training_frames_all[: int(len(self.training_frames_all) * subset_factor)]
+        self.validation_frames = self.validation_frames_all[: int(len(self.validation_frames_all) * subset_factor)]
+
+        print("length of training_frames subset:", len(self.training_frames))
+        print("length of test_frames subset:", len(self.validation_frames))
+
+        self.img_size = img_size
+        self.batch_size = batch_size
+        self.tb_path = tb_path
+
+    def load_datasets(self, num_clients: int):
+        seed = 42
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(FRAMES_IMAGE_MEAN, FRAMES_IMAGE_STD),
+            transforms.Resize((self.img_size, self.img_size))
+        ])
+
+        trainset = ZodDataset(zod_frames=self.zod_frames, frames_id_set=self.training_frames, transform=transform)
+        testset = ZodDataset(zod_frames=self.zod_frames, frames_id_set=self.validation_frames, transform=transform)
+
+        # Split training set into `num_clients` partitions to simulate different local datasets
+        partition_size = len(trainset) // num_clients
+
+        lengths = [partition_size]
+        if num_clients > 1:
+            lengths = [partition_size] * (num_clients - 1)
+            lengths.append(len(trainset) - sum(lengths))
+            
+        datasets = random_split(trainset, lengths, torch.Generator().manual_seed(seed))
+
+        # Split each partition into train/val and create DataLoader
+        trainloaders, valloaders = [], []
+        lengths_train, lengths_val = [], []
+        for ds in datasets:
+            len_val = int(len(ds) * VAL_FACTOR)
+            len_train = int(len(ds) - len_val)
+            lengths_train.append(len_train)
+            lengths_val.append(len_val)
+            lengths = [len_train, len_val]
+            ds_train, ds_val = random_split(ds, lengths, torch.Generator().manual_seed(seed))
+            trainloaders.append(DataLoader(ds_train,batch_size=self.batch_size, shuffle=True, num_workers=NUM_WORKERS))
+            valloaders.append(DataLoader(ds_val, batch_size=self.batch_size, num_workers=NUM_WORKERS))
+
+        len_complete_val = int(len(trainset) * VAL_FACTOR)
+        len_complete_train = int(len(trainset) - len_complete_val)
+        train_split, val_split = random_split(
+            trainset,
+            [len_complete_train, len_complete_val],
+            torch.Generator().manual_seed(seed),
+        )
+
+        completeTrainloader = DataLoader(train_split, batch_size=self.batch_size, num_workers=NUM_WORKERS)
+        completeValloader = DataLoader(val_split, batch_size=self.batch_size, num_workers=NUM_WORKERS)
+
+        testloader = DataLoader(testset, batch_size=self.batch_size, num_workers=NUM_WORKERS)
+
+        """report to tensor board"""
+        save_dataset_tb_plot(self.tb_path, lengths_train, "training", seed)
+        save_dataset_tb_plot(self.tb_path, lengths_val, "validation", seed)
+
+        return (
+            trainloaders,
+            valloaders,
+            testloader,
+            completeTrainloader,
+            completeValloader,
+        )
+
+class ZodDataset(Dataset):
+    def __init__(
+        self,
+        zod_frames,
+        frames_id_set,
+        transform=None,
+        target_transform=None,
+    ):
+        self.zod_frames = zod_frames
+        self.frames_id_set = frames_id_set
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.frames_id_set)
+
+    def __getitem__(self, idx):
+        frame_idx = self.frames_id_set[idx]
+        frame = self.zod_frames[frame_idx]
+
+        image = frame.get_image(Anonymization.DNAT)
+        mask = get_frame_seg_mask(self.zod_frames, frame_idx)
+
+        #label = label.astype("float32")
+        #image = image.astype("float32")
+
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            mask = self.target_transform(mask)
+
+        return image, mask
+
+
---
+++ source/FixMatchSeg/static_params.py
@@ -0,0 +1,32 @@
+from imports import *
+
+NUM_OUTPUT = 51
+IMG_SIZE = 500
+RUN_PRETRAINED = True
+BATCH_SIZE = 32
+VAL_FACTOR = 0.15
+SUBSET_FACTOR = 1
+USE_GPU = True
+NUM_GLOBAL_ROUNDS = 3
+NUM_LOCAL_EPOCHS = 10
+PRINT_DEBUG_DATA = True
+NUM_WORKERS = 10
+FRAMES_IMAGE_MEAN = [0.337, 0.345, 0.367]
+FRAMES_IMAGE_STD = [0.160, 0.180, 0.214]
+DEVICE = torch.device("cuda" if USE_GPU else "cpu")
+TRAIN_FRAMES_PATH = "GroundTruth/training_seg_annotated_frames.json"
+VAL_FRAMES_PATH = "GroundTruth/validation_seg_annotated_frames.json"
+DATASET_ROOT = "/mnt/ZOD"
+ZENSEACT_DATASET_ROOT = "/staging/dataset_donation/round_2"
+
+available_device = "cuda" if torch.cuda.is_available() else "only cpu"
+print(f"🚩{available_device} is available in this machine using PyTorch {torch.__version__} and Flower {fl.__version__}")
+print(f"🚩Pytorch vision version: {torchvision.__version__}")
+print(f"🚩training will run on: {DEVICE}s")
+
+""" path to tensor board persistent folders"""
+now = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+TB_PATH = f"TensorBoard/runs{now}"
+TB_CENTRALIZED_SUB_PATH = "TensorBoard_Centralized/loss"
+TB_FEDERATED_SUB_PATH = "TensorBoard_Federated/loss"
+TB_SWARM_SUB_PATH = "TensorBoard_Swarm/loss"
